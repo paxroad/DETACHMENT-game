0ab0e42d6b37197d1e05946a1b02d52fb294fe3d
Chan <i572125099@gmail.com>
2024-04-14 03:27:35 +0800

    Initial Commit

---
+++ 📑(Detachment).exe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  📑(Detachment).exe
+//
+//  Created by [「MACBOOK」] 1 on 14/4/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
